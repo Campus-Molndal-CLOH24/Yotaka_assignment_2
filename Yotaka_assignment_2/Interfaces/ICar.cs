122b02e40b6284843be690fe28b8db9e3c9c426f
Yotaka k. <88yotkha@gafe.molndal.se>
2024-09-25 00:06:22 +0200

    delect class car inside this interface

--- Yotaka_assignment_2/Interfaces/ICar.cs
+++ Yotaka_assignment_2/Interfaces/ICar.cs
@@ -17,42 +17,4 @@ namespace Yotaka_assignment_2.Interfaces
         string Drive();
     }
     
-    class Car : ICar, IVehicle, IDriveable
-    {
-        public string Brand { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
-        public double Mileage { get; set; }
-        public int Doors { get; set; }
-        public string EngineType { get; set; }
-        private bool engineOn;
-
-        public bool IsEngineOn()
-        {
-            return engineOn;
-        }
-
-        public void StartEngine()
-        {
-            engineOn = true;
-            Console.WriteLine("The engine is on.");
-        }
-
-        public void StopEngine()
-        {
-            engineOn = false;
-            Console.WriteLine("The engine is off.");
-        }
-
-        public string Drive()
-        {
-            return "The car is driving.";
-        }
-
-        // Explicit implementation for IVehicle interface
-        string IVehicle.IsEngineOn()
-        {
-            return engineOn ? "Engine is on" : "Engine is off";
-        }
-    }
 }
--- Yotaka_assignment_2/Models/Car.cs
+++ Yotaka_assignment_2/Models/Car.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Yotaka_assignment_2.Interfaces;
 
 namespace Yotaka_assignment_2.Models
 {
-    public class Car
+    public class Car : ICar
     {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double Mileage { get; set; }
+        public int Doors { get; set; }
+        public string EngineType { get; set; }
+        private bool engineOn;
+
+        public bool IsEngineOn()
+        {
+            return engineOn;
+        }
+
+        public void StartEngine()
+        {
+            engineOn = true;
+            Console.WriteLine("The engine is on.");
+        }
+
+        public void StopEngine()
+        {
+            engineOn = false;
+            Console.WriteLine("The engine is off.");
+        }
+
+        public string Drive()
+        {
+            return "The car is driving.";
+        }
+
+        // Explicit implementation for IVehicle interface
+        string IVehicle.IsEngineOn()
+        {
+            return engineOn ? "Engine is on" : "Engine is off";
+        }
     }
 }
