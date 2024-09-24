756ec60b004db85925f134d5f2097a271e75befa
Yotaka k. <88yotkha@gafe.molndal.se>
2024-09-25 00:05:03 +0200

    add 2 claases car and motorcycle classes

---
+++ Yotaka_assignment_2/Models/Motorcycle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yotaka_assignment_2.Models
+{
+    public class Motorcycle
+    {
+    }
+}
